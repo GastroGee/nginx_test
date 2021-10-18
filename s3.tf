220bcabbda1a77edd70386ae84e58bf5ccad7951
Ola Ogunsegha <ola.ogunsegha@code42.com>
2021-10-18 18:58:52 -0500

    update s3

--- s3.tf
+++ s3.tf
@@ -9,8 +9,8 @@ resource "aws_s3_bucket" "project" {
 }
 
 resource "aws_s3_bucket_object" "project" {
-  bucket = "project"
-  key    = "project"
+  bucket = "${aws_s3_bucket.project.arn}"
+  key    = "webconf"
   source = "${file("${path.module}/templates/web.conf")}"
 }
 
